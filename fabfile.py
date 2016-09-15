1e00d87c94a3d6a6bcdb279e3db01beac421d804
Ben Keith <ben@benlk.com>
2016-09-14 23:54:44 -0400

    Typofix in fabfile

--- fabfile.py
+++ fabfile.py
@@ -25,9 +25,9 @@ def production():
     Work on production environment
     """
     env.settings    = 'production'
-    env.hosts       = [ os.environ['CHICAVOREPORTE_PRODUCTION_SFTP_HOST'], ]    # ssh host for production.
-    env.user        = os.environ['CHICAVOREPORTE_PRODUCTION_SFTP_USER']    # ssh user for production.
-    env.password    = os.environ['CHICAVOREPORTE_PRODUCTION_SFTP_PASSWORD']    # ssh password for production.
+    env.hosts       = [ os.environ['CHICAGOREPORTE_PRODUCTION_SFTP_HOST'], ]    # ssh host for production.
+    env.user        = os.environ['CHICAGOREPORTE_PRODUCTION_SFTP_USER']    # ssh user for production.
+    env.password    = os.environ['CHICAGOREPORTE_PRODUCTION_SFTP_PASSWORD']    # ssh password for production.
     env.domain      = 'chicagoreporte.wpengine.com'
     env.port        = '2222'
 
@@ -38,9 +38,9 @@ def staging():
     Work on staging environment
     """
     env.settings    = 'staging'
-    env.hosts       = [ os.environ['CHICAVOREPORTE_STAGING_SFTP_HOST'], ]    # ssh host for staging.
-    env.user        = os.environ['CHICAVOREPORTE_STAGING_SFTP_USER']    # ssh user for staging.
-    env.password    = os.environ['CHICAVOREPORTE_STAGING_SFTP_PASSWORD']    # ssh password for staging.
+    env.hosts       = [ os.environ['CHICAGOREPORTE_STAGING_SFTP_HOST'], ]    # ssh host for staging.
+    env.user        = os.environ['CHICAGOREPORTE_STAGING_SFTP_USER']    # ssh user for staging.
+    env.password    = os.environ['CHICAGOREPORTE_STAGING_SFTP_PASSWORD']    # ssh password for staging.
     env.domain      = ''  # chicagoreporte.staging.wpengine.com
     env.port        = '2222'
 
