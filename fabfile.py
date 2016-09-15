c8503c4255a807a8da628cd840d02fbb2c442632
Ben Keith <ben@benlk.com>
2016-09-14 20:06:39 -0400

    Add creds to fabfile, add README describing setup for vvv VM host

--- fabfile.py
+++ fabfile.py
@@ -6,8 +6,10 @@ from fabric.api import task
 """
 Base configuration
 """
-env.project_name = ''       # name for the project.
+env.project_name = 'chicagoreporter'       # name for the project.
 env.hosts = ['localhost', ]
+env.sftp_deploy = True
+env.domain = 'chicagoreporter.vagrant.dev'
 
 """
 Add HipChat info to send a message to a room when new code has been deployed.
@@ -23,9 +25,11 @@ def production():
     Work on production environment
     """
     env.settings    = 'production'
-    env.hosts       = []    # ssh host for production.
-    env.user        = ''    # ssh user for production.
-    env.password    = ''    # ssh password for production.
+    env.hosts       = [ os.environ['CHICAVOREPORTE_PRODUCTION_SFTP_HOST'], ]    # ssh host for production.
+    env.user        = os.environ['CHICAVOREPORTE_PRODUCTION_SFTP_USER']    # ssh user for production.
+    env.password    = os.environ['CHICAVOREPORTE_PRODUCTION_SFTP_PASSWORD']    # ssh password for production.
+    env.domain      = 'chicagoreporte.wpengine.com'
+    env.port        = '2222'
 
 
 @task
@@ -34,9 +38,11 @@ def staging():
     Work on staging environment
     """
     env.settings    = 'staging'
-    env.hosts       = []    # ssh host for staging.
-    env.user        = ''    # ssh user for staging.
-    env.password    = ''    # ssh password for staging.
+    env.hosts       = [ os.environ['CHICAVOREPORTE_STAGING_SFTP_HOST'], ]    # ssh host for staging.
+    env.user        = os.environ['CHICAVOREPORTE_STAGING_SFTP_USER']    # ssh user for staging.
+    env.password    = os.environ['CHICAVOREPORTE_STAGING_SFTP_PASSWORD']    # ssh password for staging.
+    env.domain      = ''  # chicagoreporte.staging.wpengine.com
+    env.port        = '2222'
 
 try:
     from local_fabfile import  *
