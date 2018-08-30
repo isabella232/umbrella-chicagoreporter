a5244e67e067f254236418b83626ee1f8248d7f8
Ben Keith <ben@benlk.com>
2018-08-30 13:19:48 -0400

    Update fabfile.py for flywheel

--- fabfile.py
+++ fabfile.py
@@ -9,7 +9,7 @@ Base configuration
 env.project_name = 'chicagoreporter'       # name for the project.
 env.hosts = ['localhost', ]
 env.sftp_deploy = True
-env.domain = 'chicagoreporter.vagrant.dev'
+env.domain = 'chicagoreporter.test'
 
 """
 Add HipChat info to send a message to a room when new code has been deployed.
@@ -25,10 +25,11 @@ def production():
     Work on production environment
     """
     env.settings    = 'production'
-    env.hosts       = [ os.environ['CHICAGOREPORTE_PRODUCTION_SFTP_HOST'], ]    # ssh host for production.
-    env.user        = os.environ['CHICAGOREPORTE_PRODUCTION_SFTP_USER']    # ssh user for production.
-    env.password    = os.environ['CHICAGOREPORTE_PRODUCTION_SFTP_PASSWORD']    # ssh password for production.
-    env.domain      = 'chicagoreporte.wpengine.com'
+    env.hosts       = [ os.environ['CHICAGOREPORTER_PRODUCTION_SFTP_HOST'], ]    # ssh host for production.
+    env.path        = os.environ['CHICAGOREPORTER_PRODUCTION_SFTP_PATH']
+    env.user        = os.environ['FLYWHEEL_SFTP_USER']    # ssh user for production.
+    env.password    = os.environ['FLYWHEEL_SFTP_PASS']    # ssh password for production.
+    env.domain      = 'www.chicagoreporter.com'
     env.port        = '2222'
 
 
@@ -38,10 +39,11 @@ def staging():
     Work on staging environment
     """
     env.settings    = 'staging'
-    env.hosts       = [ os.environ['CHICAGOREPORTE_STAGING_SFTP_HOST'], ]    # ssh host for staging.
-    env.user        = os.environ['CHICAGOREPORTE_STAGING_SFTP_USER']    # ssh user for staging.
-    env.password    = os.environ['CHICAGOREPORTE_STAGING_SFTP_PASSWORD']    # ssh password for staging.
-    env.domain      = 'chicagoreporte.staging.wpengine.com'  # chicagoreporte.staging.wpengine.com
+    env.hosts       = [ os.environ['CHICAGOREPORTER_STAGING_SFTP_HOST'], ]    # ssh host for staging
+    env.path        = os.environ['CHICAGOREPORTER_STAGING_SFTP_PATH']
+    env.user        = os.environ['FLYWHEEL_SFTP_USER']    # ssh user for production.
+    env.password    = os.environ['FLYWHEEL_SFTP_PASS']    # ssh password for production.
+    env.domain      = 'staging.fast-plate.flywheelsites.com'  # chicagoreporte.staging.wpengine.com
     env.port        = '2222'
 
 try:
